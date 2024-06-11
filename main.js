2469923166f3674bca1f0350c4b54684d7a9d50f
houaslam <miraihajar16@gmail.com>
2024-06-11 18:44:24 +0100

    push

--- main.js
+++ main.js
@@ -1,100 +1,97 @@
-let submitBtn = document.getElementById("calculate-btn");
-let emptyFieldMsg = document.getElementsByClassName("empty-field");
-let invalidValMsg = document.getElementsByClassName("invalid-date");
+let submitBtn = document.getElementById( "calculate-btn" );
+let emptyFieldMsg = document.getElementsByClassName( "empty-field" );
+let invalidValMsg = document.getElementsByClassName( "invalid-date" );
 
-submitBtn.addEventListener("click", ()=>{
-    let isvalid = true;
-    let items = [];
-    items.push(document.getElementById("day"));
-    items.push(document.getElementById("month"));
-    items.push(document.getElementById("year"));
+submitBtn.addEventListener( "click", (  )=>{
+	let isvalid = true;
 
-    let day_v = items[0].value;
-    let month_v = items[1].value;
-    let year_v = items[2].value;
-    clearDisplay(items);
-    for (let index = 0; index < items.length; index++) {
-        let val = items[index].value;
-        if (!val){
-            display_error(items[index], 1, index)
-            isvalid = false;
-        }
-        else if (val <= 0){
-            display_error(items[index], 2, index)
-            isvalid = false;
-        }
-    }
-    isValid = validateDate(items[0], items[1], items[2]);
-    if (isvalid){
-        display_date(day_v, month_v, year_v)
-    }
-});
 
-function display_date(day, month, year){
-    let date = new Date();
-    let year_v = date.getFullYear() - parseInt(year);
-    let month_v = date.getMonth() - (parseInt(month) - 1);
-    let day_v = date.getDate() - parseInt(day);
-    
-    if (day_v < 0) {
-        month_v--;
-        let previousMonth = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
-        day_v += previousMonth;
-        }
-        
-        if (month_v < 0) {
-            year_v--;
-            month_v += 12;
-            }
-        document.getElementById("years").innerText = year_v;
-        document.getElementById("months").innerText = month_v;
-        document.getElementById("days").innerText = day_v;
+	let day = ( document.getElementById( "day" ) );
+	let month = ( document.getElementById( "month" ) );
+	let year = ( document.getElementById( "year" ) );
+
+
+	clearDisplay( [day, month, year]);
+	console.log(day.value);
+	isvalid = check_empty(  day, month, year );
+	if (isvalid)
+		isvalid = check_value(  day, month, year );
+	if (isvalid)
+		isvalid = check_date(  day, month, year );
+	if ( isvalid == true ){
+	    display_date( day.value, month.value, year.value )
+	}
+} );
+
+function check_empty(day, month, year){
+	if (!day.value)
+		return display_error(day, 1, 0);
+	if (!month.value)
+		return display_error(month, 1,1);
+	if (!year.value)
+		return display_error(year, 1, 2);
+	return true;
 }
 
-function validateDate(day, month, year) {
-
-    let day_v = parseInt(day.value);
-    let month_v = parseInt(month.value);
-    let year_v = parseInt(year.value);
-
-    let date = new Date(year_v,month_v - 1,day_v);
-
-    console.log(date);
-
-    if (date.getFullYear() !== year_v){
-        console.error("YEAR");
-        display_error(year, 2, 0);
-        return false;
-    }
-    if (date.getMonth() !== month_v -1){
-        console.error("MONTH");
-        display_error(month, 2, 0);
-        return false;
-    }
-    if (date.getDate()!== day_v){
-        console.error("DAY");
-        display_error(day, 2, 0);
-        return false;
-    }
-    return true;
+function check_value(day, month, year){
+	let nowYear = new Date().getFullYear();
+	if (day.value <= 0 || day.value > 32)
+		return display_error(day, 2, 0);
+	if (month.value <= 0 || month.value > 13)
+		return display_error(month, 2,1);
+	if (year.value <= 0 || year.value > nowYear)
+		return display_error(year, 2, 2);
+	return true;
 }
 
-function display_error(item, type, index){
-    item.style.border = '1px solid var(--Light-red)';
-    if (type == 1){
-        emptyFieldMsg[index].style.display = 'block';
-    }
-    if(type == 2){
-        invalidValMsg[index].style.display = 'block';
+function check_date(day, month, year){
+	let date = new Date(`${day}-${month}-${year}`);
+	let res = date.getFullYear() === year.value && date.getMonth() === month.value + 1 && date.getDate() === day.value;
+	if (!res){
+		display_error(day, 1, 0);
+		display_error(month, 0, 1);
+		return display_error(year, 0, 2);
+	}
+}
 
-    }
+
+function display_date( day, month, year ){
+	let date = new Date(  );
+	let year_v = date.getFullYear(  ) - parseInt( year );
+	let month_v = date.getMonth(  ) - ( parseInt( month ) - 1 );
+	let day_v = date.getDate(  ) - parseInt( day );
+	
+	if ( day_v < 0 ) {
+		month_v--;
+		let previousMonth = new Date( date.getFullYear(  ), date.getMonth(  ), 0 ).getDate(  );
+		day_v += previousMonth;
+		}
+		
+		if ( month_v < 0 ) {
+			year_v--;
+			month_v += 12;
+			}
+		document.getElementById( "years" ).innerText = year_v;
+		document.getElementById( "months" ).innerText = month_v;
+		document.getElementById( "days" ).innerText = day_v;
+}
+
+
+function display_error( item, type, index ){
+	item.style.border = '1px solid var( --Light-red )';
+	if ( type == 1 ){
+		emptyFieldMsg[index].style.display = 'block';
+	}
+	if( type == 2 ){
+		invalidValMsg[index].style.display = 'block';
+	}
+	return false;
 }   
 
-
-function clearDisplay(items){
-    for (let index = 0; index < items.length; index++) {
-        items[index].style.border = '1px solid var(--Light-grey)';
-         emptyFieldMsg[index].style.display = 'none';
-        invalidValMsg[index].style.display = 'none';
-    }
+function clearDisplay( items ){
+	for ( let index = 0; index < items.length; index++ ) {
+		items[index].style.border = '1px solid var( --Light-grey )';
+		 emptyFieldMsg[index].style.display = 'none';
+		invalidValMsg[index].style.display = 'none';
+	}
 }
